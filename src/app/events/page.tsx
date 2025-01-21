b1dbd99620de157ce6555b6c872af8288022c828
Harsh Gajjar <harshgajjar2812@gmail.com>
2025-01-22 02:45:21 +0530

    update(events): add event

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,40 +1,20 @@
 import Image from "next/image";
 
-const upcomingEvents = [
-  {
-    id: 1,
-    title: "Dev-o-lution",
-    type: "National Event",
-    description:
-      "Dev-o-lution isa tech conference where coding meets creativity, and innovation breaks free from convention. Join us for workshops, talks, and mind-expanding sessions that will revolutionize your dev skills!",
-    date: "19th Jan 2025",
-    image: "/devolution.png",
-  },
-];
+// const upcomingEvents = [];
 
 const events = [
   {
     id: 1,
-    title: "GSoC Intro Session",
-    type: "Intra College Session",
+    title: "TechLoop",
+    type: "Intra College Event",
     description:
-      "The GSoC Intro event served as an orientation for students interested in participating in Google Summer of Code (GSoC). It provided insights into open-source contributions, tips for selecting the right projects and organizations, and strategies for crafting strong proposals to increase their chances of selection.",
-    date: "14th Nov 2024",
+      "Techloop was a comprehensive event focused on exploring various domains of technology, providing participants with an overview of fields like web development, AI, blockchain, cybersecurity, and more. It featured interactive sessions, hands-on workshops, and live demos to help attendees discover their interests and gain foundational knowledge in diverse tech areas.",
+    date: "15th Oct 2024",
     image:
-      "https://raw.githubusercontent.com/ossdaiict/dsc-website/4de7e4d9cc70dacf486a7b93936abb59a2bdb113/assets/images/events/card/GSoC-icon.svg",
+      "https://github.com/ossdaiict/dsc-website/blob/master/assets/images/events/card/techloop.png?raw=true",
   },
   {
     id: 2,
-    title: "SLoP 4.0",
-    type: "National Event",
-    description:
-      "Slop 4.0 was organized as part of Hacktoberfest, celebrating open-source contributions through a collaborative hackathon. Participants worked on real-world projects, submitted pull requests, and gained hands-on experience with version control, fostering a culture of collaboration and learning in the open-source community.",
-    date: "25th Sep - 10th Dec 2024",
-    image:
-      "https://github.com/ossdaiict/dsc-website/blob/master/assets/images/events/card/slop4.png?raw=true",
-  },
-  {
-    id: 3,
     title: "CodeLabs",
     type: "Intra College Event",
     description:
@@ -44,21 +24,43 @@ const events = [
       "https://raw.githubusercontent.com/ossdaiict/dsc-website/4de7e4d9cc70dacf486a7b93936abb59a2bdb113/assets/images/events/card/web%20dev.svg",
   },
   {
-    id: 4,
-    title: "TechLoop",
-    type: "Intra College Event",
+    id: 3,
+    title: "SLoP 4.0",
+    type: "National Event",
     description:
-      "Techloop was a comprehensive event focused on exploring various domains of technology, providing participants with an overview of fields like web development, AI, blockchain, cybersecurity, and more. It featured interactive sessions, hands-on workshops, and live demos to help attendees discover their interests and gain foundational knowledge in diverse tech areas.",
-    date: "15th Oct 2024",
+      "Slop 4.0 was organized as part of Hacktoberfest, celebrating open-source contributions through a collaborative hackathon. Participants worked on real-world projects, submitted pull requests, and gained hands-on experience with version control, fostering a culture of collaboration and learning in the open-source community.",
+    date: "25th Sep - 10th Dec 2024",
     image:
-      "https://github.com/ossdaiict/dsc-website/blob/master/assets/images/events/card/techloop.png?raw=true",
+      "https://github.com/ossdaiict/dsc-website/blob/master/assets/images/events/card/slop4.png?raw=true",
+    url: "https://slop.dscdaiict.in/",
+  },
+  {
+    id: 4,
+    title: "GSoC Intro Session",
+    type: "Intra College Session",
+    description:
+      "The GSoC Intro event served as an orientation for students interested in participating in Google Summer of Code (GSoC). It provided insights into open-source contributions, tips for selecting the right projects and organizations, and strategies for crafting strong proposals to increase their chances of selection.",
+    date: "14th Nov 2024",
+    image:
+      "https://raw.githubusercontent.com/ossdaiict/dsc-website/4de7e4d9cc70dacf486a7b93936abb59a2bdb113/assets/images/events/card/GSoC-icon.svg",
+  },
+  {
+    id: 5,
+    title: "Dev-o-lution",
+    type: "National Event",
+    description:
+      "Dev-o-lution is a tech conference where coding meets creativity, and innovation breaks free from convention. Join us for workshops, talks, and mind-expanding sessions that will revolutionize your dev skills!",
+    date: "19th Jan 2025",
+    image:
+      "https://github.com/gdg-da/devolution/blob/main/public/planet.png?raw=true",
+    url: "https://devolution.dscdaiict.in/",
   },
 ];
 
 export default function Events() {
   return (
     <div>
-      <div className="mb-12">
+      {/* <div className="mb-12">
         <h1 className="text-5xl font-black mb-12 text-center comic-text animate-slideInFromLeft">
           Upcoming Events
         </h1>
@@ -93,32 +95,44 @@ export default function Events() {
             </div>
           ))}
         </div>
-      </div>
+      </div> */}
       <div>
         <h1 className="text-5xl font-black mb-12 text-center comic-text animate-slideInFromLeft">
           Past Events
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {events.map((event, index) => (
-            <div
-              key={event.id}
-              className="comic-panel animate-scaleIn"
-              style={{ animationDelay: `${index * 0.1}s` }}>
-              <div className="comic-border mb-4 overflow-hidden flex justify-center">
-                <Image
-                  src={event.image}
-                  alt={event.title}
-                  width={300}
-                  height={200}
-                  className="h-[200px] object-contain"
-                />
+          {events
+            .sort((a, b) => b.id - a.id) // Sort events by ID in descending order
+            .map((event, index) => (
+              <div
+                key={event.id}
+                className="comic-panel animate-scaleIn"
+                style={{ animationDelay: `${index * 0.1}s` }}>
+                <div className="comic-border mb-4 overflow-hidden flex justify-center">
+                  <Image
+                    src={event.image}
+                    alt={event.title}
+                    width={300}
+                    height={200}
+                    className="h-[200px] object-contain"
+                  />
+                </div>
+                {event.url ? (
+                  <a href={event.url} target="_blank" rel="noopener noreferrer">
+                    <h2 className="comic-yellow comic-text mb-2">
+                      {event.title}
+                    </h2>
+                  </a>
+                ) : (
+                  <h2 className="comic-yellow comic-text mb-2">
+                    {event.title}
+                  </h2>
+                )}
+                <p className="text-lg mb-2">{event.type}</p>
+                <p className="text-lg mb-4">{event.description}</p>
+                <p className="text-lg mb-6">Date: {event.date}</p>
               </div>
-              <h2 className="comic-yellow comic-text mb-2">{event.title}</h2>
-              <p className="text-lg mb-2">{event.type}</p>
-              <p className="text-lg mb-4">{event.description}</p>
-              <p className="text-lg mb-6">Date: {event.date}</p>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </div>
